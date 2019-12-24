ca85f2e0e3d95447177a3206c0e96891d2cca378
priestfteast <htcp3450@mail.ru>
2019-12-24 17:54:54 +0300

    Added add and update pet functionality. closes #56

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/PetType.java
@@ -1,22 +1,33 @@
 package guru.springframework.sfgpetclinic.model;
 
+
 import lombok.*;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
 
-@Getter
+/**
+ * Created by jt on 7/13/18.
+ */
+
 @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
+
 
     @Override
     public String toString() {
