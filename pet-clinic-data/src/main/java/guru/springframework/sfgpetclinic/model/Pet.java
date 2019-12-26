906b3a9125f7edbb4c7cfc65e802011adfcb3642
priestfteast <htcp3450@mail.ru>
2019-12-26 11:52:34 +0300

    Added add and update visit functionality. closes #57 closes#53

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -1,32 +1,53 @@
 package guru.springframework.sfgpetclinic.model;
 
 import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import javax.persistence.*;
 import java.time.LocalDate;
 import java.util.HashSet;
 import java.util.Set;
 
-@Getter
+/**
+ * Created by jt on 7/13/18.
+ */
 @Setter
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor
-@Builder
 @Entity
 @Table(name = "pets")
-public class Pet  extends BaseEntity{
+public class Pet extends BaseEntity{
+
+    @Builder
+    public Pet(Long id, String name, PetType petType, Owner owner, LocalDate birthDate, Set<Visit> visits) {
+        super(id);
+        this.name = name;
+        this.petType = petType;
+        this.owner = owner;
+        this.birthDate = birthDate;
+
+        if (visits == null || visits.size() > 0 ) {
+            this.visits = visits;
+        }
+    }
 
     @Column(name = "name")
     private String name;
+
     @ManyToOne
     @JoinColumn(name = "type_id")
     private PetType petType;
+
     @ManyToOne
     @JoinColumn(name = "owner_id")
     private Owner owner;
-    @Column(name = "birthday")
+
+    @Column(name = "birth_date")
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private LocalDate birthDate;
-    @OneToMany(cascade = CascadeType.ALL,mappedBy = "pet")
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "pet")
     private Set<Visit> visits = new HashSet<>();
 
 }
